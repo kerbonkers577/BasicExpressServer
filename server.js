80abac2a18d923e68eb0c16046eac3687b5c7bd1
Tristan Constable <tristan.m.constable@gmail.com>
2020-07-28 13:17:07 +0200

    API points setup and get and post http methods working with MongoDB connection

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
+const http = require('http');
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 let Post = require ('./post_model');
 
 mongoose.connect('mongodb://localhost/Blog',{
@@ -24,4 +26,42 @@ app.get('/', function (req, res){
     
 })
 
-app.listen(3000);
+//Initialized here as to not force GET headers to expect JSON
+app.use(bodyParser.json());
+
+app.post('/add', function (req, res){
+    
+    const blogPost = new Post({
+        title: req.body.title,
+        author: req.body.author,
+        date: req.body.date,
+        content: req.body.content
+    });
+
+    blogPost.save((err, blogPost) => {
+        if(err)
+        {
+            res.send("Error saving information");
+        }
+        else{
+            res.send("Blog Post saved")
+        }
+    })
+    
+})
+
+//Should probably put the bottom two routes towards an id
+// e.g. /delete(or update)/id/
+
+app.put('/', function (req, res){
+    console.log("Update query logged");
+})
+
+app.delete('/', function (req, res){
+    console.log("Delete query logged");
+})
+
+//This hosts an http app through Node's event listeners
+app.listen(3000, () => {
+    console.log("App connected and listening at PORT 3000 on this machines IPv4 Address");
+});
